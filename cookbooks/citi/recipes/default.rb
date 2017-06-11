cdaadf13be1ad472bf446cae3c9c4ec72f1559b1
root <root@chefwork.ryan.com>
2017-06-10 21:25:24 -0400

    modifed the default.rb

--- cookbooks/citi/recipes/default.rb
+++ cookbooks/citi/recipes/default.rb
@@ -6,7 +6,11 @@
 #include_recipe "citi::book6"
 #include_recipe "citi::book7"
 #include_recipe "citi::book8"
-include_recipe "citi::book9"
+#include_recipe "citi::book9"
 #include_recipe "citi::book10"
 #include_recipe "citi::tree"
 #include_recipe "citi::nginx"
+
+file '/tmp/newfile' do
+	action :create
+end
